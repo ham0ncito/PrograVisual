ae5d0f1d3678719a37e98d13bde4a3b0236a798c
ham0ncito <lil_ham0n@hotmail.com>
2022-07-11 22:17:36 -0600

    login

--- Juan_Piece_RestauranteOtaku/frLogin.Designer.vb
+++ Juan_Piece_RestauranteOtaku/frLogin.Designer.vb
@@ -56,7 +56,7 @@ Partial Class frLogin
         '
         Me.TextBox2.Location = New System.Drawing.Point(158, 86)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(56)
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
         Me.TextBox2.Size = New System.Drawing.Size(215, 25)
         Me.TextBox2.TabIndex = 3
         '
