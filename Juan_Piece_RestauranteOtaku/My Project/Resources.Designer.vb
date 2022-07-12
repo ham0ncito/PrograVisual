40542c6096d67f4d54a419f607fee7fcf29f2b7b
ham0ncito <lil_ham0n@hotmail.com>
2022-07-11 22:42:14 -0600

    mod

--- Juan_Piece_RestauranteOtaku/My Project/Resources.Designer.vb
+++ Juan_Piece_RestauranteOtaku/My Project/Resources.Designer.vb
@@ -63,6 +63,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Public ReadOnly Property Copy_of_Happy_Hour() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Copy of Happy Hour", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Public ReadOnly Property Happy_Hour() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Happy Hour", resourceCulture)
