1349f64aa3ea87d61a1b2b68dddbefce858f0908
ham0ncito <lil_ham0n@hotmail.com>
2022-07-11 23:43:58 -0600

    user

--- Juan_Piece_RestauranteOtaku/My Project/Resources.Designer.vb
+++ Juan_Piece_RestauranteOtaku/My Project/Resources.Designer.vb
@@ -103,16 +103,6 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
-        Public ReadOnly Property One_Pice() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("One Pice", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        '''</summary>
         Public ReadOnly Property One_Pice__1__PhotoRoom__1_() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("One Pice (1)-PhotoRoom (1)", resourceCulture)
