fecc69bb3c9a57d991bb6c1741dbb1a209a43a81
ham0ncito <lil_ham0n@hotmail.com>
2022-07-27 10:39:05 -0600

    hoySiEsElUltimo

--- Juan_Piece_RestauranteOtaku/My Project/Resources.Designer.vb
+++ Juan_Piece_RestauranteOtaku/My Project/Resources.Designer.vb
@@ -173,6 +173,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Public ReadOnly Property ramen_de_pollo_receta_facil_f_1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("ramen-de-pollo-receta-facil-f-1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Public ReadOnly Property ramen_de_pollo_receta_facil_f_1_PhotoRoom() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("ramen-de-pollo-receta-facil-f-1-PhotoRoom", resourceCulture)
