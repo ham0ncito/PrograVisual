c00060ef8704ec6ef0ccaf016d3d8f555181bf62
ham0ncito <lil_ham0n@hotmail.com>
2022-07-26 19:31:09 -0600

    ANTESDELDESASTRE

--- Juan_Piece_RestauranteOtaku/My Project/Application.Designer.vb
+++ Juan_Piece_RestauranteOtaku/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Juan_Piece_RestauranteOtaku.frLogin
+            Me.MainForm = Global.Juan_Piece_RestauranteOtaku.Form1
         End Sub
     End Class
 End Namespace
